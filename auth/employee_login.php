abe3d1d3d948c4e19d8d208a92e56411e7b34ed8
Pedro <up201705008@fe.up.pt>
2020-12-12 23:24:36 +0000

    Css for register and login pages

--- auth/employee_login.php
+++ auth/employee_login.php
@@ -1,16 +1,19 @@
 <?php include('../templates/header.php') ?>
-<section id="login">
-    <h2>Empregados | Login</h2>
-    <form action="action_employeeLogin.php" method="post">
-        <label for="name">Nome de Utilizador</label>
-        <input type="text" placeholder="" name="name" required>
-        <br>
-        <label for="password">Password</label>
-        <input type="password" placeholder="" name="password" required>
-        <br>
-        <input type="submit" value="Entrar">
-
-        <a href="employee_register.php">Novo Registo</a>
-    </form>
-</section>
+<div class="row">
+    <section class="column">
+        <h2>Empregados | Login</h2>
+        <form action="action_employeeLogin.php" method="post">
+            <label for="name">Username</label>
+            <input type="text" placeholder="" name="name" required>
+            <br>
+            <label for="password">Password</label>
+            <input type="password" placeholder="" name="password" required>
+            <br>
+            <input type="submit" value="Entrar">
+        </form>
+    </section>
+    <section class="column">
+        <a href="employee_register.php" class="button">Novo Registo</a>
+    </section>
+</div>
 <?php include('../templates/footer.php') ?>
