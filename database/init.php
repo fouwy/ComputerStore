9396e3d8ff6024719da3b71afbab17294ca8031d
Pedro <up201705008@fe.up.pt>
2020-12-23 15:30:07 +0000

    "Fix" db init in login

--- database/init.php
+++ database/init.php
@@ -1,6 +1,17 @@
 <?php
-    $dbh = new PDO('sqlite:./sql/ComputerStore.db');
-	$dbh->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    //deve haver melhor maneira de fazer isto
+    try {
+        $dbh = new PDO('sqlite:./sql/ComputerStore.db');
+    } catch (PDOException $e) {
+        try {
+            $dbh = new PDO('sqlite:../sql/ComputerStore.db');
+        } catch (PDOException $e2) {
+            echo $e;
+            echo $e2;
+            die();
+        }
+    }
+    $dbh->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
     $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
     session_start();
