a1c3440475b92206ae959ad0a3c0ff95607354ff
Miguel <up201705172@fe.up.pt>
2020-12-28 22:36:23 +0000

    Added Frontpage photo/text +Readme

--- index.php
+++ index.php
@@ -7,7 +7,12 @@
 </ul>
 </nav>
 <section class="image">
-    <h2>About Us</h2>
+<h2>About Us</h2>
+    <h3>We are a small computer store located at Rua Doutor Roberto Frias </h3>
+    <h3>  To get a service on your computer , please register first on our website as a client
+    </h3>
+    <img src=img/pc-repair-tech-crew-f.jpg alt="Guy repairing a PC" >
+
 </section>
 <section></section>
 
