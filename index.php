576a82987eb22796272702aa6ed01991454d4d92
Pedro <up201705008@fe.up.pt>
2020-12-28 02:24:33 +0000

    surprise

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <ul>
     <li><a href="auth/client_login.php">I'm a client</a></li>
     <li><a href="auth/employee_login.php">I'm an employee</a></li>
-    <li><a href="img/pic.png">surprise</a></li>
+    <li><a id = "hidden" href="img/pic.png">surprise</a></li>
 </ul>
 </nav>
 <section class="image"></section>
